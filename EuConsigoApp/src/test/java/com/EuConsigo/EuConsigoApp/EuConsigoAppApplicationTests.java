442ae582de0e9a45f1dfb248ff764205fa840c7b
nicollasprado <nicollasprado.profissional@gmail.com>
2025-01-30 17:33:54 -0300

    Configurações do spring, conexão com o postgres e adição da versão das dependências

---
+++ EuConsigoApp/src/test/java/com/EuConsigo/EuConsigoApp/EuConsigoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EuConsigo.EuConsigoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuConsigoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
